b994f580b2f0b60e10410c1dc594997eec98dfe1
Sanderdehaartgit <sanderdehaart@gmail.com>
2025-02-25 12:19:22 +0100

    Added the layout of the post details page.

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -1,29 +1,120 @@
 import 'package:flutter/material.dart';
+import '../widgets/description_info_widget.dart';
+import '../widgets/labels_widget.dart';
+import '../widgets/comment_widget.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   const DetailsPage({super.key});
+
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _selectedIndex = index;
+      });
+      Navigator.pop(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Details Page'),
-      ),
-      body: Center(
+      backgroundColor: Colors.grey[200],
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'This is the Details Page',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the HomePage
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back to Home'),
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 280,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://picsum.photos/200/300?random=1'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Container(color: Colors.black.withOpacity(0.3)),
+                ),
+                Positioned(
+                  top: 40,
+                  left: 20,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  right: 20,
+                  child: IconButton(
+                    icon: const Icon(Icons.more_vert, color: Colors.white),
+                    onPressed: () {},
+                  ),
+                ),
+                Positioned(
+                  top: 230,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35),
+                        topRight: Radius.circular(35),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20),
+                        const DescriptionInfoWidget(),
+                        const SizedBox(height: 20),
+                        const LabelsWidget(),
+                        const SizedBox(height: 25),
+                        const Text(
+                          'Comments (3)',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Column(
+                          children: const [
+                            CommentWidget(
+                              username: 'Sophie Lady',
+                              comment:
+                                  "I totally agree with you! It's sad how little people think about the environment.",
+                            ),
+                            CommentWidget(
+                              username: 'Mark Dawson',
+                              comment:
+                                  "Yes, it's frustrating! Maybe we can organize a clean-up?",
+                            ),
+                            CommentWidget(
+                              username: 'Emily Victoria',
+                              comment:
+                                  "Good point! I'll spread the word in my community.",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 50),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
