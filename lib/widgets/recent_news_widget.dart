f4774dfd61137c35ce1035cf6ea1052d14fdb8f1
Sanderdehaartgit <sanderdehaart@gmail.com>
2025-02-25 12:20:33 +0100

    Added navigation for each post (card).

--- lib/widgets/recent_news_widget.dart
+++ lib/widgets/recent_news_widget.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../screens/details_page.dart';
 
 class RecentNewsWidget extends StatelessWidget {
   @override
@@ -29,112 +29,121 @@ class RecentNewsWidget extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: 10, // Number of news items
               itemBuilder: (context, index) {
-                return Container(
-                  width: 225,
-                  margin: EdgeInsets.only(
-                    left: index == 0 ? 30 : 4, // 30px margin for the first card
-                  ),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DetailsPage()),
+                    );
+                  },
+                  child: Container(
+                    width: 225,
+                    margin: EdgeInsets.only(
+                      left:
+                          index == 0 ? 30 : 4, // 30px margin for the first card
                     ),
-                    clipBehavior: Clip.antiAlias,
-                    child: Stack(
-                      children: [
-                        // Background image
-                        Positioned.fill(
-                          child: Image.network(
-                            "https://picsum.photos/200/300?random=$index",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-
-                        // Green overlay
-                        Positioned.fill(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color(0xff2F9675).withOpacity(0.3),
-                                  Color(0xff87CFA2).withOpacity(0.3),
-                                ],
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                              ),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      clipBehavior: Clip.antiAlias,
+                      child: Stack(
+                        children: [
+                          // Background image
+                          Positioned.fill(
+                            child: Image.network(
+                              "https://picsum.photos/200/300?random=$index",
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        ),
 
-                        // Content inside the card
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
-                          child: Container(
-                            padding: const EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Colors.black.withOpacity(0.0),
-                                  Colors.black.withOpacity(0.9),
-                                ],
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                              ),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                // Title
-                                Text(
-                                  "News Title $index",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-
-                                SizedBox(height: 4), // Slight spacing
-
-                                // Description
-                                Text(
-                                  "This is a short description of the news article. Stay updated!",
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.normal,
-                                    fontFamily: "Montserrat",
-                                  ),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-
-                                SizedBox(height: 35), // Space before comments
-
-                                // Comments Section
-                                Row(
-                                  children: [
-                                    Icon(Icons.comment,
-                                        color: Colors.white, size: 18),
-                                    SizedBox(width: 6),
-                                    Text(
-                                      "${(index + 1) * 5} Comments",
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.normal,
-                                        fontFamily: "Montserrat",
-                                      ),
-                                    )
+                          // Green overlay
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xff2F9675).withOpacity(0.3),
+                                    Color(0xff87CFA2).withOpacity(0.3),
                                   ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
                                 ),
-                              ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+
+                          // Content inside the card
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              padding: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Colors.black.withOpacity(0.0),
+                                    Colors.black.withOpacity(0.9),
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                ),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  // Title
+                                  Text(
+                                    "News Title $index",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+
+                                  SizedBox(height: 4), // Slight spacing
+
+                                  // Description
+                                  Text(
+                                    "This is a short description of the news article. Stay updated!",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.normal,
+                                      fontFamily: "Montserrat",
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+
+                                  SizedBox(height: 35), // Space before comments
+
+                                  // Comments Section
+                                  Row(
+                                    children: [
+                                      Icon(Icons.comment,
+                                          color: Colors.white, size: 18),
+                                      SizedBox(width: 6),
+                                      Text(
+                                        "${(index + 1) * 5} Comments",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.normal,
+                                          fontFamily: "Montserrat",
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
